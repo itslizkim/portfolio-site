543ffda65df5b296c704d68a93f655e5219d2b46
itslizkim <elizabeth.kim93@gmail.com>
2021-10-26 17:45:55 -0400

    added resume pdf to ResumePage

---
+++ src/components/resumeComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ResumePDF from '../images/tech_resume.pdf'
+
+const ResumeComponent = () => {
+    return (
+        <div className='pdf-container' >
+            <object className='resumePdf'
+                data={ResumePDF}
+                type='application/pdf'
+                title='Liz Kim Resume 2021'
+                aria-label='Liz Kim Tech Resume'
+                style={{border: '1', color: 'black'}}
+                width='670'
+                height='950'
+            />
+        </div>
+    )
+}
+
+export default ResumeComponent
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import ResumeComponent from '../components/ResumeComponent';
 
 const ResumePage = () => {
     return (
-        <Layout pageTitle="Resume">
-
+        <Layout pageTitle='Resume'>
+            <div className='resume-section'>
+                <ResumeComponent/>
+            </div>
         </Layout>
     )
 }
