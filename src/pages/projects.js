b4b5ba9402828e3de4841420dd5d0287fa07f233
itslizkim <elizabeth.kim93@gmail.com>
2021-10-26 16:12:33 -0400

    added navlinks for projects and resume

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout'
+
+const ProjectPage = () => {
+    return (
+        <Layout pageTitle="Projects">
+            <p>Projects page</p>
+        </Layout>
+    )
+} 
+
+export default ProjectPage
---
+++ src/pages/resume.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout'
+
+const ResumePage = () => {
+    return (
+        <Layout pageTitle="Resume">
+
+        </Layout>
+    )
+}
+
+export default ResumePage
